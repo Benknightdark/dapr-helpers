1acdb931e998ad57c7c7085e8601dc0464475482
ben <moon791017@gmail.com>
2021-12-06 16:03:21 +0800

    調整invoke_api.py

--- dapr_httpx/invoke_api.py
+++ dapr_httpx/invoke_api.py
@@ -4,9 +4,61 @@ from dapr_httpx.error_log_helper import format_error_msg
 
 
 class InvokeApi(DaprApi):
+    service_name = ''
+    invoke_url = ''
 
-    async def get_bulk_secrets(self, secret_store_name):
-        return
+    def __init__(self, service_name):
+        self.service_name = service_name
+        self.invoke_url = f'{self.api_url}/invoke/{self.service_name}/{self.secret_store_name}/method'
 
-    async def get_secert(self, secret_store_name, name):
-        return
+    async def get(self, method_name):
+        '''
+        發送get request
+        '''
+        try:
+            req = await self.client.get(f'{self.invoke_url}/{method_name}')
+            res = req.json()
+            return res
+        except Exception as e:
+            logging.error(format_error_msg(e))
+        finally:
+            await self.client.aclose()
+
+    async def post(self, method_name, payload):
+        '''
+        發送post request
+        '''
+        try:
+            req = await self.client.post(f'{self.invoke_url}/{method_name}', json=payload)
+            res = req.json()
+            return res
+        except Exception as e:
+            logging.error(format_error_msg(e))
+        finally:
+            await self.client.aclose()
+
+    async def put(self, method_name, payload):
+        '''
+        發送put request
+        '''
+        try:
+            req = await self.client.put(f'{self.invoke_url}/{method_name}', json=payload)
+            res = req.json()
+            return res
+        except Exception as e:
+            logging.error(format_error_msg(e))
+        finally:
+            await self.client.aclose()
+
+    async def delete(self, method_name, payload):
+        '''
+        發送delete request
+        '''
+        try:
+            req = await self.client.delete(f'{self.invoke_url}/{method_name}', json=payload)
+            res = req.json()
+            return res
+        except Exception as e:
+            logging.error(format_error_msg(e))
+        finally:
+            await self.client.aclose()
--- run/run.py
+++ run/run.py
@@ -1,4 +1,4 @@
 from dapr_httpx.secrets_api import SecretsApi
 import asyncio
 
-asyncio.run(SecretsApi(timeout=5,retries=0).get_bulk_secrets('a'))
+print(asyncio.run(SecretsApi(timeout=5,retries=0).get_bulk_secrets('a')))
