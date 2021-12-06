dcf8675bfdbaff8f014d8e54ffa3bc71fcc9cc98
ben <moon791017@gmail.com>
2021-12-06 17:28:32 +0800

    update secrets api

--- dapr_httpx/secrets_api.py
+++ dapr_httpx/secrets_api.py
@@ -7,28 +7,34 @@ class SecretsApi(DaprApi):
     '''
     Secrets API
     '''
+
+    def __init__(self):
+        DaprApi.__init__(self)
+
     async def get_bulk_secrets(self, secret_store_name):
         '''
         批次取得秘密資料
         '''
         try:
+            self.start_client()
             req = await self.client.get(f'{self.api_url}/secrets/{secret_store_name}/bulk')
             res = req.json()
             return res
         except Exception as e:
             logging.error(format_error_msg(e))
         finally:
-            await self.client.aclose()
+            await self.close_client()
 
     async def get_secert(self, secret_store_name, name):
         '''
         根據秘密儲存庫名稱取得對應的秘密資料
         '''
         try:
+            self.start_client()
             req = await self.client.get(f'{self.api_url}/secrets/{secret_store_name}/{name}')
             res = req.json()
             return res
         except Exception as e:
             logging.error(format_error_msg(e))
         finally:
-            await self.client.aclose()
+            await self.close_client()
